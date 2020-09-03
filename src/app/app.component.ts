0016178ba8cc62cf4ce6a732b5c4252056c43207
Shaina Koval <skoval@aiimi.com>
2020-09-03 16:52:59 +0100

    🦄 Install guided tour with basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {GuidedTour, Orientation, GuidedTourModule, GuidedTourService} from 'ngx-guided-tour';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-guided-tour-experiment';
+
+
+  public dashboardTour: GuidedTour = {
+    tourId: 'experiment-tour',
+    useOrb: false,
+    steps: [
+        {
+            title: 'Welcome to the Guided Tour Demo',
+            selector: '#stepOne',
+            content: 'Step 1',
+            orientation: Orientation.Bottom
+        },
+        {
+            title: 'Positioning',
+            selector: '#stepTwo',
+            content: 'Step position can be set so that steps are always in view. This step is on the left.',
+            orientation: Orientation.Left
+        },
+        {
+            title: 'Positioning 2',
+            selector: '#stepThree',
+            content: 'This step is on the right.',
+            orientation: Orientation.Right
+        },
+        {
+            title: 'Scroll to content',
+            selector: '#stepFour',
+            content: 'Automatically scroll to elements so they are in view',
+            orientation: Orientation.Top
+        }
+    ]
+  };
+
+  constructor(
+      private guidedTourService: GuidedTourService
+  ) {
+
+      setTimeout(() => {
+          this.guidedTourService.startTour(this.dashboardTour);
+      }, 1000);
+  }
+
+  public onTourStart(): void {
+      this.guidedTourService.startTour(this.dashboardTour);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {GuidedTourModule, GuidedTourService} from 'ngx-guided-tour';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GuidedTourModule
   ],
-  providers: [],
+  providers: [GuidedTourService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
